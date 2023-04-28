14808793121462e9053af53a92fb1a7de4cd5b01
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2023-04-28 09:46:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+var player1 = document.querySelector(".player--0");
+var player2 = document.querySelector(".player--1");
+var score1 = document.querySelector("#score--0");
+var score2 = document.querySelector("#score--1");
+var current0 = document.querySelector("#current--0");
+var current1 = document.querySelector("#current--1");
+
+var dice = document.querySelector(".dice");
+var btnNew = document.querySelector(".btn--new");
+var btnRoll = document.querySelector(".btn--roll");
+var btnHold = document.querySelector(".btn--hold");
+
+//player1.classList.add("player--active ");
+// roll dice
+
+let activeplayer, scores, currentScore;
+
+function rolldice() {
+  dice.style.display = "block";
+  /* dice.src =
+    "https://d22ueo28hfk252.cloudfront.net/Content/versioned/2.0.0.1/images/version4/promotion_march_23/zenrik_images/dice-1-16780282692122.png?v=1678028269"; */
+  diceno = Math.floor(Math.random() * 6) + 1;
+  console.log(diceno);
+  if (diceno == 1) {
+    dice.src =
+      "https://d22ueo28hfk252.cloudfront.net/Content/versioned/2.0.0.1/images/version4/promotion_march_23/zenrik_images/dice-1-16780282692122.png?v=1678028269";
+  } else if (diceno == 2) {
+    dice.src =
+      "https://d22ueo28hfk252.cloudfront.net/Content/versioned/2.0.0.1/images/version4/promotion_march_23/zenrik_images/dice-2-16780282681882.png?v=1678028268";
+  } else if (diceno == 3) {
+    dice.src =
+      "https://d22ueo28hfk252.cloudfront.net/Content/versioned/2.0.0.1/images/version4/promotion_march_23/zenrik_images/dice-3-16780282673568.png?v=1678028267";
+  } else if (diceno == 4) {
+    dice.src =
+      "https://d22ueo28hfk252.cloudfront.net/Content/versioned/2.0.0.1/images/version4/promotion_march_23/zenrik_images/dice-4-16780282710131.png?v=1678028271";
+  } else if (diceno == 5) {
+    dice.src =
+      "https://d22ueo28hfk252.cloudfront.net/Content/versioned/2.0.0.1/images/version4/promotion_march_23/zenrik_images/dice-5-16780248143120.png?v=1678024814";
+  } else if (diceno == 6) {
+    dice.src =
+      "https://d22ueo28hfk252.cloudfront.net/Content/versioned/2.0.0.1/images/version4/promotion_march_23/zenrik_images/dice-6-16780282700231.png?v=1678028270";
+  }
+
+  if (diceno !== 1) {
+    currentScore += diceno;
+    document.getElementById(`current--${activeplayer}`).innerText =
+      currentScore;
+  } else {
+    switchplayer();
+  }
+}
+document.onreadystatechange = () => {
+  dice.style.display = "None";
+  score1.innerHTML = 0;
+  score2.innerHTML = 0;
+  currentScore = 0;
+};
+
+function switchplayer() {
+  document.getElementById(`current--${activeplayer}`).textContent = 0;
+  currentScore = 0;
+  activeplayer = activeplayer === 0 ? 1 : 0;
+  player1.classList.toggle("player--active");
+  player2.classList.toggle("player--active");
+}
+
+//switchplayer();
+
+//hold button
+
+function initialState() {
+  score = [0, 0];
+  currentScore = 0;
+  activeplayer = 0;
+  score1.textContent = 0;
+  score2.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
+  dice.style.display = "None";
+
+  // let currentScore = 0;
+  // alert("hello");
+}
+
+document.querySelector(".btn--hold").addEventListener("click", onholdState);
+document.querySelector(".btn--new").addEventListener("click", initialState);
+
+/* function onreadystatechange() {
+  console.log("domeready active");
+} */
+
+/* document.onreadystatechange();
+console.log("window loaded");
+ */
+
+//initialState();
+function onholdState() {
+  score[activeplayer] += currentScore;
+  console.log(score[activeplayer]);
+  document.getElementById(`score--${activeplayer}`).textContent =
+    score[activeplayer];
+  currentScore = 0;
+}
